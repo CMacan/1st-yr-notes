8be063162586cbed35ff356508bf8cbf26f9bd75
CMacan <113482858+CMacan@users.noreply.github.com>
2022-09-16 15:52:58 +0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+
+printf ("Please enter a number:");
+scanf("%d", &n);
+
+if (n%2==0)
+printf("The number %d is even", n);
+
+else
+printf("The number %d is odd", n);
+
+return 0;
+}
