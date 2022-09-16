9bbdca165dda114ad17843fb9e0a4921cb42ce3c
CMacan <113482858+CMacan@users.noreply.github.com>
2022-09-16 16:06:45 +0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int num1,num2, sum=0;
+
+printf("Enter a number:");
+scanf("%d", &num1);
+
+printf("Enter 2nd number:");
+scanf("%d", &num2);
+
+sum=num1+num2;
+
+printf("The sum of both numbers is %d", sum);
+
+return 0;
+}
